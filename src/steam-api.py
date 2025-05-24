c2451b510b74813bec026a8cfe060b751478cf86
SereniT33 <serenachoi1992@gmail.com>
2025-05-24 15:13:22 -0700

    Implement GCS bucket upload functionality and set up service account key

--- src/steam-api.py
+++ src/steam-api.py
@@ -4,6 +4,8 @@ from avro.datafile import DataFileWriter
 from avro.io import DatumWriter
 import avro.schema
 from pathlib import Path
+from google.cloud import storage
+import os
 
 DATA_PATH = Path(__file__).resolve().parent / "data"
 CATALOGUE_JSON_PATH = DATA_PATH / "catalogue.json"
@@ -18,26 +20,52 @@ CATALOGUE_AVRO_PATH = DATA_PATH / "calalogue.avro"
 # with open(CATALOGUE_JSON_PATH, "w") as f:
 #     json.dump(response.json(), f)
 
-# Load JSON data
-with open(CATALOGUE_JSON_PATH, "r") as f:
-    json_data = json.load(f)
+# # Load JSON data
+# with open(CATALOGUE_JSON_PATH, "r") as f:
+#     json_data = json.load(f)
 
-# Extract the list of apps
-apps = json_data["applist"]["apps"]
+# # Extract the list of apps
+# apps = json_data["applist"]["apps"]
 
-# Define Avro schema
-schema = avro.schema.parse(json.dumps({
-    "type": "record",
-    "name": "App",
-    "fields": [
-        {"name": "appid", "type": "long"},
-        {"name": "name", "type": "string"}
-    ]
-}))
+# # Define Avro schema
+# schema = avro.schema.parse(json.dumps({
+#     "type": "record",
+#     "name": "App",
+#     "fields": [
+#         {"name": "appid", "type": "long"},
+#         {"name": "name", "type": "string"}
+#     ]
+# }))
 
-# Write to Avro
-with open(CATALOGUE_AVRO_PATH, 'wb') as out:
-    writer = DataFileWriter(out, DatumWriter(), schema)
-    for app in apps:
-        writer.append(app)
-    writer.close()
+# # Write to Avro
+# with open(CATALOGUE_AVRO_PATH, 'wb') as out:
+#     writer = DataFileWriter(out, DatumWriter(), schema)
+#     for app in apps:
+#         writer.append(app)
+#     writer.close()
+
+
+# Set up GCS Bucket connection
+
+# Path to the service account key
+SERVICE_ACCOUNT_KEY_PATH = Path(__file__).resolve().parent.parent / "Credentials" / "service-key.json"
+
+# Set up GCS Bucket connection
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(SERVICE_ACCOUNT_KEY_PATH)
+
+# Uploading an object
+def upload_to_bucket(bucket_name, source_file_name, destination_blob_name):
+    # Initialize a storage client
+    client = storage.Client(project="steamed-bunz",credentials=SERVICE_ACCOUNT_KEY_PATH)
+
+    # Get the bucket
+    bucket = client.bucket(bucket_name)
+
+    # Upload the file
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+bucket_name = "steamed-bunz-extract"
+upload_to_bucket(bucket_name, CATALOGUE_AVRO_PATH, "steam-catalogue/current.avro")
